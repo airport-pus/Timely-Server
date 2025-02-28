384b086fe9ce9217f1c18ecb727a1188b4859e18
GSB0203 <gimseungbin0203@gmail.com>
2025-02-28 19:10:53 +0900

    feat(Init): 기본 설정

---
+++ src/test/kotlin/com/timely/TimelyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.timely
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TimelyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
