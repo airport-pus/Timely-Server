903aaf874a10c2154caf9e20e85af5351531af45
GSB0203 <gimseungbin0203@gmail.com>
2025-03-02 01:56:57 +0900

    feat(Swagger): TimetableController
    스웨거를 추가하였습니다.

--- src/main/kotlin/com/timely/domain/timetable/controller/TimetableController.kt
+++ src/main/kotlin/com/timely/domain/timetable/controller/TimetableController.kt
@@ -3,16 +3,23 @@ package com.timely.domain.timetable.controller
 import com.timely.domain.timetable.controller.dto.TimetableRequest
 import com.timely.domain.timetable.controller.dto.TimetableResponse
 import com.timely.domain.timetable.service.TimetableService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 @RestController
+@Tag(name = "Timetable API", description = "학교 시간표 API")
 class TimetableController(private val timetableService: TimetableService) {
 
     @PostMapping("/timetable")
-    suspend fun getTime(@RequestBody requset: TimetableRequest): Mono<TimetableResponse> {
-        return timetableService.getTimetable(requset)
+    @Operation(summary = "시간표 조회", description = "학교 시간표를 조회합니다.")
+    suspend fun getTime(
+        @Parameter(description = "시간표 조회 요청 데이터", required = true) @RequestBody request: TimetableRequest
+    ): Mono<TimetableResponse> {
+        return timetableService.getTimetable(request)
     }
 }
