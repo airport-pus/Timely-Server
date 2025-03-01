cbfaf89c11b0b7720e6ea56c997f82593c3f4f94
GSB0203 <gimseungbin0203@gmail.com>
2025-03-02 01:57:09 +0900

    feat(Swagger): TimetableResponse
    스웨거를 추가하였습니다.

--- src/main/kotlin/com/timely/domain/timetable/controller/dto/TimetableResponse.kt
+++ src/main/kotlin/com/timely/domain/timetable/controller/dto/TimetableResponse.kt
@@ -3,8 +3,11 @@ package com.timely.domain.timetable.controller.dto
 import com.timely.domain.timetable.service.dto.ApiElementaryTimeResponse
 import com.timely.domain.timetable.service.dto.ApiMiddleTimetableResponse
 import com.timely.domain.timetable.service.dto.ApiHighTimetableResponse
+import io.swagger.v3.oas.annotations.media.Schema
 
+@Schema(description = "시간표 응답 데이터")
 data class TimetableResponse(
+    @Schema(description = "시간표 리스트", required = true)
     val timeSummary: List<Timetable>
 ) {
     companion object {
@@ -55,8 +58,14 @@ data class TimetableResponse(
     }
 }
 
+@Schema(description = "시간표 항목")
 data class Timetable(
+    @Schema(description = "시간표 날짜", example = "20250301", required = true)
     val allTiYmd: String,
+
+    @Schema(description = "교시", example = "1", required = true)
     val perio: String,
+
+    @Schema(description = "수업 내용", example = "수학", required = true)
     val itrtCntnt: String
 )
