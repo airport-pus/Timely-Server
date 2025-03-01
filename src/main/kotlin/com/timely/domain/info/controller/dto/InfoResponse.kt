8468d16103508f534abda714fc9101e89825d978
GSB0203 <gimseungbin0203@gmail.com>
2025-03-02 01:56:51 +0900

    feat(Swagger): InfoResponse
    스웨거를 추가하였습니다.

--- src/main/kotlin/com/timely/domain/info/controller/dto/InfoResponse.kt
+++ src/main/kotlin/com/timely/domain/info/controller/dto/InfoResponse.kt
@@ -1,10 +1,17 @@
 package com.timely.domain.info.controller.dto
 
 import com.timely.domain.info.service.dto.ApiInfoResponse
+import io.swagger.v3.oas.annotations.media.Schema
 
+@Schema(description = "학교 정보 응답 데이터")
 data class InfoResponse(
+    @Schema(description = "시도 교육청 코드", example = "C10", required = true)
     val ATPT_OFCDC_SC_CODE: String,
+
+    @Schema(description = "행정 표준 코드", example = "7150658", required = true)
     val SD_SCHUL_CODE: String,
+
+    @Schema(description = "학교 종류 이름", example = "고등학교", required = true)
     val SCHUL_KND_SC_NM: String
 ) {
     companion object {
