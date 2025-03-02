0693acf527a61259542aabe3898d4fd40799ef49
GSB0203 <gimseungbin0203@gmail.com>
2025-03-02 12:34:41 +0900

    refactor(Info): InfoResponse
    명칭을 수정하였습니다.

--- src/main/kotlin/com/timely/domain/info/controller/dto/InfoResponse.kt
+++ src/main/kotlin/com/timely/domain/info/controller/dto/InfoResponse.kt
@@ -6,13 +6,13 @@ import io.swagger.v3.oas.annotations.media.Schema
 @Schema(description = "학교 정보 응답 데이터")
 data class InfoResponse(
     @Schema(description = "시도 교육청 코드", example = "C10", required = true)
-    val ATPT_OFCDC_SC_CODE: String,
+    val atptOfcdcScCode: String,
 
     @Schema(description = "행정 표준 코드", example = "7150658", required = true)
-    val SD_SCHUL_CODE: String,
+    val sdSchulCode: String,
 
     @Schema(description = "학교 종류 이름", example = "고등학교", required = true)
-    val SCHUL_KND_SC_NM: String
+    val schulKndScNm: String
 ) {
     companion object {
         fun from(apiResponse: ApiInfoResponse): InfoResponse? {
@@ -21,9 +21,9 @@ data class InfoResponse(
 
             return row?.row?.firstOrNull()?.let {
                 InfoResponse(
-                    ATPT_OFCDC_SC_CODE = it.ATPT_OFCDC_SC_CODE ?: "",
-                    SD_SCHUL_CODE = it.SD_SCHUL_CODE ?: "",
-                    SCHUL_KND_SC_NM = it.SCHUL_KND_SC_NM ?: ""
+                    atptOfcdcScCode = it.ATPT_OFCDC_SC_CODE ?: "",
+                    sdSchulCode = it.SD_SCHUL_CODE ?: "",
+                    schulKndScNm = it.SCHUL_KND_SC_NM ?: ""
                 )
             }
         }
